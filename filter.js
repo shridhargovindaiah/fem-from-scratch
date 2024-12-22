0f46d706942a401a86599310debf879772b97084
Shridhar <Shridhar.Govindaiah@gmail.com>
2024-12-22 15:33:46 -0600

    Page 2 completed

---
+++ filter.js
@@ -0,0 +1,9 @@
+const cards = document.querySelectorAll('.mushroom-guide .card');
+const seasonalFilter = document.querySelector('#seson');
+const edibleFilter = document.querySelector('#edible');
+
+const currentFilter = {
+    season: 'all',
+    edible: 'all'
+};
+
